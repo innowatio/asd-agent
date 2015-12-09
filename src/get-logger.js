6f6f007091cd977e96938587af4caf97914c3253
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-12-09 11:52:33 +0100

    Ensure AGENT_LOG_PATH directory exists.

--- src/get-logger.js
+++ src/get-logger.js
@@ -1,8 +1,10 @@
 import bunyan from "bunyan";
+import fs from "fs-extra";
 
 import * as c from "./config";
 
 export default function getLogger (name) {
+    fs.mkdirpSync(c.AGENT_LOG_PATH);
     return bunyan.createLogger({
         name,
         streams: [
